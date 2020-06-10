6274391744848bb13005c994b5bf68a39c28f9f0
peawarrior <jacksonchen171@gmail.com>
2020-06-10 13:07:20 -0400

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,18 @@
 def nyc_pigeon_organizer(data)
   # write your code here!
+  revised_data = {}
+  data.each do |category, category_value|
+    category_value.each do |attribute, names|
+      names.each do |name|
+        if !revised_data[name]
+          revised_data[name] = {}
+        end
+        if !revised_data[name][category]
+          revised_data[name][category] = []
+        end
+        revised_data[name][category] << attribute.to_s
+      end
+    end
+  end
+  revised_data
 end
